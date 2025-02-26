e8a659a4b590a45bfdb314361cb2f39d0bec64cb
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-27 06:47:49 +1100

    Update route.ts

--- app/api/genai/route.ts
+++ app/api/genai/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request, res: Response) {
   const prompt = reqBody.data.prompt;
 
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
   const streamingResponse = await model.generateContentStream(prompt);
   return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse));
 }
